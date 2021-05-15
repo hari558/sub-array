f62ee6da299022698b60cd62cb38b441fa9cd74e
hari558 <84020240+hari558@users.noreply.github.com>
2021-05-15 22:41:10 +0530

    Create SubbarryXOR.java

---
+++ SubbarryXOR.java
@@ -0,0 +1,44 @@
+class GFG {
+
+    static int maxSubarrayXOR(int arr[], int n)
+
+    {
+
+        int ans = Integer.MIN_VALUE;
+
+        for (int i=0; i<n; i++)
+
+       {  
+
+            int curr_xor = 0; 
+
+            for (int j=i; j<n; j++)
+
+            {
+
+                curr_xor = curr_xor ^ arr[j];
+
+                ans = Math.max(ans, curr_xor);
+
+            }
+
+        }
+
+        return ans;
+
+    }
+
+    public static void main(String args[])
+
+    {
+
+        int arr[] = {1, 2, 3, 4};
+
+        int n = arr.length;
+
+        System.out.println("Max subarray XOR is " + 
+
+                                 maxSubarrayXOR(arr, n));
+
+    }
+}
