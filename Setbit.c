968c9535d03c6f2697448937262d2c3ae712ca9b
hari558 <84020240+hari558@users.noreply.github.com>
2021-05-15 22:31:54 +0530

    Create Setbit.c

---
+++ Setbit.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+unsigned int countSetBits(int n)
+{
+
+    unsigned int count = 0;
+
+    while (n) {
+
+        n &= (n - 1);
+
+        count++;
+
+    }
+
+    return count;
+}
+
+int main()
+{
+
+    int i = 9;
+
+    printf("%d", countSetBits(i));
+
+    getchar();
+
+    return 0;
+}
